82c13d1f546d62897f1dce9efe81b22d0a0a7cb0
Youssef Habri <youssef.habri@gmail.com>
2018-11-08 21:45:09 +0100

    Added more lua commands

--- scripts/main.lua
+++ scripts/main.lua
@@ -9,4 +9,11 @@
 local discord = require 'discord'
 discord.message("bestgirl", "It's Clearly me. <:02smug:490438406174801921> What were you expecting?! <:bakabaka:490438407747534858>")
 discord.message("bestboy", "What's that, I can't hear you ...")
+
 discord.message("lion", "**Lionirdeadman** was a very dear member of this server. Current status: MIA")
+discord.message("rem", "https://www.youtube.com/watch?v=QdZT8xswjJA")
+
+discord.message("darling", "My one & only darling, @YoussefHabri#5084!")
+discord.message("makemeasandwich", "I only make sandwiches for my darling!")
+discord.message("iloveyou", "I only love my darling. But we can still be friends, ok?")
+discord.message("array", {"1", "2"})
