1f402ba20a3bbaaf70f01ee0ea5d96d324c959d8
Youssef Habri <youssef.habri@gmail.com>
2018-11-09 17:52:07 +0100

    Added more Lua commands (Array command)

--- scripts/main.lua
+++ scripts/main.lua
@@ -13,7 +13,14 @@ discord.message("bestboy", "What's that, I can't hear you ...")
 discord.message("lion", "**Lionirdeadman** was a very dear member of this server. Current status: MIA")
 discord.message("rem", "https://www.youtube.com/watch?v=QdZT8xswjJA")
 
-discord.message("darling", "My one & only darling, @YoussefHabri#5084!")
+discord.message("darling", "My one & only darling, <@139360031102599168>!")
 discord.message("makemeasandwich", "I only make sandwiches for my darling!")
 discord.message("iloveyou", "I only love my darling. But we can still be friends, ok?")
-discord.message("array", {"1", "2"})
+
+quotes = {
+    "\"It is nice to be important, but it's more important to be nice.\" ― John Templeton",
+    "\"Are donuts holes just empty space or do they exist as an abstraction of nothing?\" ―  Miyama, Itsuki",
+    "\"People can change through their encounters. It's never too late.\" ―  Mei Tachibana",
+    "\"There's a grain of truth in every joke.\" ― Unknown"
+}
+discord.message("quotes", quotes)
